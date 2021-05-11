19a357573f65e859789dde83fb7b6f77f64b766a
ayman-metwally2020 <ayman.abdelsattary@gmail.com>
2021-05-11 22:45:03 +0200

    Add files via upload

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,27 +1,88 @@
 import sys
-
+import nltk
+import numpy as np
+nltk.download(['punkt', 'wordnet'])
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.tree import DecisionTreeClassifier
+import pickle
 
 def load_data(database_filepath):
-    pass
-
+    """Load the filepath and return the data"""
+    name = 'sqlite:///' + database_filepath
+    engine = create_engine(name)
+    df = pd.read_sql_table('FigureEight', con=engine) # is table always called this? 
+    print(df.head())
+    X = df['message']
+    y = df[df.columns[4:]]
+    category_names = y.columns
+    return X, y, category_names
 
 def tokenize(text):
-    pass
-
+    """tokenize and transform input text. Return cleaned text"""
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    # take out all punctuation while tokenizing
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+    
+    # lemmatize as shown in the lesson
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 def build_model():
-    pass
+    """Return Grid Search model with pipeline and Classifier"""
+    moc = MultiOutputClassifier(RandomForestClassifier())
 
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', moc)
+        ])
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    parameters = {'clf__estimator__max_depth': [10, 50, None],
+              'clf__estimator__min_samples_leaf':[2, 5, 10]}
 
+    cv = GridSearchCV(pipeline, parameters)
+    return cv
+    
+def evaluate_model(model, X_test, y_test, category_names):
+    """Print model results
+    INPUT
+    model -- required, estimator-object
+    X_test -- required
+    y_test -- required
+    category_names = required, list of category strings
+    OUTPUT
+    None
+    """
+    # Get results and add them to a dataframe.
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=category_names))
+    results = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
 
 def save_model(model, model_filepath):
-    pass
-
+    """Save model as pickle file"""
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 def main():
+    """Load the data, run the model and save model"""
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
